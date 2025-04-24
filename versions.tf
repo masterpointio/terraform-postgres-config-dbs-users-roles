7428b375721b51a6e05451917145d0ca1f9b69c7
Weston Platter <westonplatter+m22@gmail.com>
2025-04-24 13:43:34 -0600

    Update versions.tf
    Co-authored-by: Veronika Gnilitska <30597968+gberenice@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     postgresql = {
       source  = "cyrilgdn/postgresql"
-      version = "~> 1"
+      version = ">= 1"
     }
     random = {
       source  = "hashicorp/random"
