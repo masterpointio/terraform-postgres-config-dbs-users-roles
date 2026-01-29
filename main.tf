30881230848067732f2be6881ed0332c45b87116
Weston Platter <weston.platter@masterpoint.io>
2026-01-29 16:35:08 -0700

    feat: declare role dependencies. base roles -> intermediate -> derived roles

--- main.tf
+++ main.tf
@@ -31,6 +31,33 @@ locals {
   roles_map = { for role in local._roles_with_passwords : role.role.name => role }
 
   databases_map = { for database in var.databases : database.name => database }
+
+  # Built-in PostgreSQL roles that don't need to be created by this module
+  builtin_roles = [
+    "pg_monitor",
+    "pg_read_all_data",
+    "pg_write_all_data",
+    "pg_read_all_settings",
+    "pg_read_all_stats",
+    "pg_stat_scan_tables", "pg_signal_backend", "pg_read_server_files",
+    "pg_write_server_files", "pg_execute_server_program", "pg_checkpoint", "pg_maintain",
+    "pg_create_subscription", "pg_use_reserved_connections"
+  ]
+
+  # All custom role names being created by this module
+  custom_role_names = [for role in local._roles_with_passwords : role.role.name]
+
+  # Base roles: no `roles` attribute, or only referencing built-in PostgreSQL roles
+  base_roles_map = {
+    for role in local._roles_with_passwords : role.role.name => role
+    if try(role.role.roles, null) == null || alltrue([for r in coalesce(role.role.roles, []) : contains(local.builtin_roles, r)])
+  }
+
+  # Dependent roles: roles that reference other custom roles defined in this module
+  dependent_roles_map = {
+    for role in local._roles_with_passwords : role.role.name => role
+    if try(role.role.roles, null) != null && anytrue([for r in coalesce(role.role.roles, []) : contains(local.custom_role_names, r)])
+  }
 }
 
 resource "random_password" "user_password" {
@@ -51,10 +78,9 @@ resource "postgresql_database" "logical_dbs" {
   connection_limit = each.value.connection_limit
 }
 
-# In Postgres 15, now new users cannot create tables or write data to Postgres public schema by default. You have to grant create privilege to the new user manually.
-# https://www.postgresql.org/docs/current/ddl-priv.html#DDL-PRIV-CREATE
-resource "postgresql_role" "role" {
-  for_each = local.roles_map
+# Base roles: roles with no dependencies on other custom roles (created first)
+resource "postgresql_role" "base_role" {
+  for_each = local.base_roles_map
 
   name                      = each.value.role.name
   superuser                 = each.value.role.superuser
@@ -78,6 +104,32 @@ resource "postgresql_role" "role" {
   depends_on = [postgresql_database.logical_dbs]
 }
 
+# Dependent roles: roles that inherit from other custom roles (created after base roles)
+resource "postgresql_role" "dependent_role" {
+  for_each = local.dependent_roles_map
+
+  name                      = each.value.role.name
+  superuser                 = each.value.role.superuser
+  create_database           = each.value.role.create_database
+  create_role               = each.value.role.create_role
+  inherit                   = each.value.role.inherit
+  login                     = each.value.role.login
+  replication               = each.value.role.replication
+  bypass_row_level_security = each.value.role.bypass_row_level_security
+  connection_limit          = each.value.role.connection_limit
+  encrypted_password        = each.value.role.encrypted_password
+  password                  = each.value.role.password
+  roles                     = each.value.role.roles
+  search_path               = each.value.role.search_path
+  valid_until               = each.value.role.valid_until
+  skip_drop_role            = each.value.role.skip_drop_role
+  skip_reassign_owned       = each.value.role.skip_reassign_owned
+  statement_timeout         = each.value.role.statement_timeout
+  assume_role               = each.value.role.assume_role
+
+  depends_on = [postgresql_database.logical_dbs, postgresql_role.base_role]
+}
+
 resource "postgresql_default_privileges" "privileges" {
   # Postgres documentation specific to default privileges
   # https://www.postgresql.org/docs/current/sql-alterdefaultprivileges.html
@@ -91,7 +143,7 @@ resource "postgresql_default_privileges" "privileges" {
   object_type = each.value.object_type
   privileges  = each.value.privileges
 
-  depends_on = [postgresql_database.logical_dbs, postgresql_role.role]
+  depends_on = [postgresql_database.logical_dbs, postgresql_role.base_role, postgresql_role.dependent_role]
 }
 
 resource "postgresql_grant" "database_access" {
@@ -102,7 +154,7 @@ resource "postgresql_grant" "database_access" {
   object_type = each.value.object_type
   privileges  = each.value.privileges
 
-  depends_on = [postgresql_database.logical_dbs, postgresql_role.role]
+  depends_on = [postgresql_database.logical_dbs, postgresql_role.base_role, postgresql_role.dependent_role]
 }
 
 resource "postgresql_grant" "schema_access" {
@@ -114,7 +166,7 @@ resource "postgresql_grant" "schema_access" {
   object_type = each.value.object_type
   privileges  = each.value.privileges
 
-  depends_on = [postgresql_database.logical_dbs, postgresql_role.role]
+  depends_on = [postgresql_database.logical_dbs, postgresql_role.base_role, postgresql_role.dependent_role]
 }
 
 resource "postgresql_grant" "table_access" {
@@ -139,6 +191,6 @@ resource "postgresql_grant" "sequence_access" {
   object_type = each.value.object_type
   privileges  = each.value.privileges
 
-  depends_on = [postgresql_database.logical_dbs, postgresql_role.role]
+  depends_on = [postgresql_database.logical_dbs, postgresql_role.base_role, postgresql_role.dependent_role]
 }
 
